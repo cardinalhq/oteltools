4bd4087b3241349641fddda0545bc6bc8f8c9fbd
Michael Graff <explorer@flame.org>
2025-02-25 02:30:50 -0600

    improve metric generation with descriptions and units

--- pkg/ottl/telemetry.go
+++ pkg/ottl/telemetry.go
@@ -47,25 +47,60 @@ type Telemetry struct {
 func NewTelemetry(meter metric.Meter) *Telemetry {
 	ret := &Telemetry{}
 
-	if cec, err := telemetry.NewDeferrableInt64Counter(meter, "ottl.conditions.error.count", nil, nil); err == nil {
+	if cec, err := telemetry.NewDeferrableInt64Counter(meter,
+		"ottl.conditions.error.count",
+		[]metric.Int64CounterOption{
+			metric.WithDescription("The number of times condition evaluation errored."),
+			metric.WithUnit("1"),
+		}, nil); err == nil {
 		ret.ConditionsErrorCounter = cec
 	}
-	if cec, err := telemetry.NewDeferrableInt64Counter(meter, "ottl.conditions.evaluated.count", nil, nil); err == nil {
+	if cec, err := telemetry.NewDeferrableInt64Counter(meter,
+		"ottl.conditions.evaluated.count",
+		[]metric.Int64CounterOption{
+			metric.WithDescription("The number of times conditions were evaluated."),
+			metric.WithUnit("1"),
+		}, nil); err == nil {
 		ret.ConditionsEvaluatedCounter = cec
 	}
-	if ceh, err := telemetry.NewDeferrableHistogram(meter, "ottl.conditions.evaluated.time", nil, nil); err == nil {
+	if ceh, err := telemetry.NewDeferrableHistogram(meter,
+		"ottl.conditions.evaluated.time",
+		[]metric.Int64HistogramOption{
+			metric.WithDescription("The timing of conditions evaluated."),
+			metric.WithUnit("ns"),
+		}, nil); err == nil {
 		ret.ConditionsEvaluatedHistogram = ceh
 	}
-	if rlc, err := telemetry.NewDeferrableInt64Counter(meter, "ottl.rate_limited.count", nil, nil); err == nil {
+	if rlc, err := telemetry.NewDeferrableInt64Counter(meter,
+		"ottl.rate_limited.count",
+		[]metric.Int64CounterOption{
+			metric.WithDescription("The number of times conditions passed, but the statements were not executed due to rate limiting."),
+			metric.WithUnit("1"),
+		}, nil); err == nil {
 		ret.RateLimitedCounter = rlc
 	}
-	if sec, err := telemetry.NewDeferrableInt64Counter(meter, "ottl.statements.error.count", nil, nil); err == nil {
+	if sec, err := telemetry.NewDeferrableInt64Counter(meter,
+		"ottl.statements.error.count",
+		[]metric.Int64CounterOption{
+			metric.WithDescription("The number of times statement processing errored."),
+			metric.WithUnit("1"),
+		}, nil); err == nil {
 		ret.StatementsErrorCounter = sec
 	}
-	if seh, err := telemetry.NewDeferrableHistogram(meter, "ottl.statements.executed.time", nil, nil); err == nil {
+	if seh, err := telemetry.NewDeferrableHistogram(meter,
+		"ottl.statements.executed.time",
+		[]metric.Int64HistogramOption{
+			metric.WithDescription("The timing of statements executed."),
+			metric.WithUnit("ns"),
+		}, nil); err == nil {
 		ret.StatementsExecutedHistogram = seh
 	}
-	if sec, err := telemetry.NewDeferrableInt64Counter(meter, "ottl.statements.executed.count", nil, nil); err == nil {
+	if sec, err := telemetry.NewDeferrableInt64Counter(meter,
+		"ottl.statements.executed.count",
+		[]metric.Int64CounterOption{
+			metric.WithDescription("The number of times statements were executed."),
+			metric.WithUnit("1"),
+		}, nil); err == nil {
 		ret.StatementsExecutedCounter = sec
 	}
 
