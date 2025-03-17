76721badf66a8d67b8e05b58b0a874b2473b30c1
Michael Graff <mgraff@cardinalhq.io>
2025-03-17 03:04:42 -0500

    add list wrapper

--- pkg/graph/graphpb/packaged_object.pb.go
+++ pkg/graph/graphpb/packaged_object.pb.go
@@ -150,6 +150,50 @@ func (*PackagedObject_SecretSummary) isPackagedObject_Object() {}
 
 func (*PackagedObject_ConfigMapSummary) isPackagedObject_Object() {}
 
+type PackagedObjectList struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Items         []*PackagedObject      `protobuf:"bytes,1,rep,name=items,proto3" json:"items,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *PackagedObjectList) Reset() {
+	*x = PackagedObjectList{}
+	mi := &file_packaged_object_proto_msgTypes[1]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *PackagedObjectList) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PackagedObjectList) ProtoMessage() {}
+
+func (x *PackagedObjectList) ProtoReflect() protoreflect.Message {
+	mi := &file_packaged_object_proto_msgTypes[1]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PackagedObjectList.ProtoReflect.Descriptor instead.
+func (*PackagedObjectList) Descriptor() ([]byte, []int) {
+	return file_packaged_object_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *PackagedObjectList) GetItems() []*PackagedObject {
+	if x != nil {
+		return x.Items
+	}
+	return nil
+}
+
 var File_packaged_object_proto protoreflect.FileDescriptor
 
 var file_packaged_object_proto_rawDesc = string([]byte{
@@ -193,11 +237,15 @@ var file_packaged_object_proto_rawDesc = string([]byte{
 	0x65, 0x73, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01,
 	0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c,
 	0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a,
-	0x02, 0x38, 0x01, 0x42, 0x08, 0x0a, 0x06, 0x6f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x42, 0x33, 0x5a,
-	0x31, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x61, 0x72, 0x64,
-	0x69, 0x6e, 0x61, 0x6c, 0x68, 0x71, 0x2f, 0x6f, 0x74, 0x65, 0x6c, 0x74, 0x6f, 0x6f, 0x6c, 0x73,
-	0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x67, 0x72, 0x61, 0x70, 0x68, 0x2f, 0x67, 0x72, 0x61, 0x70, 0x68,
-	0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x02, 0x38, 0x01, 0x42, 0x08, 0x0a, 0x06, 0x6f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x22, 0x43, 0x0a,
+	0x12, 0x50, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x64, 0x4f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x4c,
+	0x69, 0x73, 0x74, 0x12, 0x2d, 0x0a, 0x05, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x18, 0x01, 0x20, 0x03,
+	0x28, 0x0b, 0x32, 0x17, 0x2e, 0x67, 0x72, 0x61, 0x70, 0x68, 0x70, 0x62, 0x2e, 0x50, 0x61, 0x63,
+	0x6b, 0x61, 0x67, 0x65, 0x64, 0x4f, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x52, 0x05, 0x69, 0x74, 0x65,
+	0x6d, 0x73, 0x42, 0x33, 0x5a, 0x31, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
+	0x2f, 0x63, 0x61, 0x72, 0x64, 0x69, 0x6e, 0x61, 0x6c, 0x68, 0x71, 0x2f, 0x6f, 0x74, 0x65, 0x6c,
+	0x74, 0x6f, 0x6f, 0x6c, 0x73, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x67, 0x72, 0x61, 0x70, 0x68, 0x2f,
+	0x67, 0x72, 0x61, 0x70, 0x68, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 })
 
 var (
@@ -212,26 +260,28 @@ func file_packaged_object_proto_rawDescGZIP() []byte {
 	return file_packaged_object_proto_rawDescData
 }
 
-var file_packaged_object_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_packaged_object_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_packaged_object_proto_goTypes = []any{
-	(*PackagedObject)(nil),   // 0: graphpb.PackagedObject
-	nil,                      // 1: graphpb.PackagedObject.ResourceAttributesEntry
-	nil,                      // 2: graphpb.PackagedObject.RecordAttributesEntry
-	(*PodSummary)(nil),       // 3: graphpb.PodSummary
-	(*SecretSummary)(nil),    // 4: graphpb.SecretSummary
-	(*ConfigMapSummary)(nil), // 5: graphpb.ConfigMapSummary
+	(*PackagedObject)(nil),     // 0: graphpb.PackagedObject
+	(*PackagedObjectList)(nil), // 1: graphpb.PackagedObjectList
+	nil,                        // 2: graphpb.PackagedObject.ResourceAttributesEntry
+	nil,                        // 3: graphpb.PackagedObject.RecordAttributesEntry
+	(*PodSummary)(nil),         // 4: graphpb.PodSummary
+	(*SecretSummary)(nil),      // 5: graphpb.SecretSummary
+	(*ConfigMapSummary)(nil),   // 6: graphpb.ConfigMapSummary
 }
 var file_packaged_object_proto_depIdxs = []int32{
-	1, // 0: graphpb.PackagedObject.resource_attributes:type_name -> graphpb.PackagedObject.ResourceAttributesEntry
-	2, // 1: graphpb.PackagedObject.record_attributes:type_name -> graphpb.PackagedObject.RecordAttributesEntry
-	3, // 2: graphpb.PackagedObject.pod_summary:type_name -> graphpb.PodSummary
-	4, // 3: graphpb.PackagedObject.secret_summary:type_name -> graphpb.SecretSummary
-	5, // 4: graphpb.PackagedObject.config_map_summary:type_name -> graphpb.ConfigMapSummary
-	5, // [5:5] is the sub-list for method output_type
-	5, // [5:5] is the sub-list for method input_type
-	5, // [5:5] is the sub-list for extension type_name
-	5, // [5:5] is the sub-list for extension extendee
-	0, // [0:5] is the sub-list for field type_name
+	2, // 0: graphpb.PackagedObject.resource_attributes:type_name -> graphpb.PackagedObject.ResourceAttributesEntry
+	3, // 1: graphpb.PackagedObject.record_attributes:type_name -> graphpb.PackagedObject.RecordAttributesEntry
+	4, // 2: graphpb.PackagedObject.pod_summary:type_name -> graphpb.PodSummary
+	5, // 3: graphpb.PackagedObject.secret_summary:type_name -> graphpb.SecretSummary
+	6, // 4: graphpb.PackagedObject.config_map_summary:type_name -> graphpb.ConfigMapSummary
+	0, // 5: graphpb.PackagedObjectList.items:type_name -> graphpb.PackagedObject
+	6, // [6:6] is the sub-list for method output_type
+	6, // [6:6] is the sub-list for method input_type
+	6, // [6:6] is the sub-list for extension type_name
+	6, // [6:6] is the sub-list for extension extendee
+	0, // [0:6] is the sub-list for field type_name
 }
 
 func init() { file_packaged_object_proto_init() }
@@ -253,7 +303,7 @@ func file_packaged_object_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_packaged_object_proto_rawDesc), len(file_packaged_object_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
