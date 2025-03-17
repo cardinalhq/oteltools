0b8330a20a31cc5128a3bd2272f14226fb550055
Michael Graff <explorer@flame.org>
2025-03-17 02:13:24 -0500

    Graphpb (#25)
    * protos for k8s graph objects

---
+++ pkg/graph/graphpb/doc.go
@@ -0,0 +1,21 @@
+// Copyright 2024-2025 CardinalHQ, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:generate sh -c "protoc -I . --go_out=. --go_opt=paths=source_relative *.proto"
+
+// Package graphpb holds source and generated code for the graph objects that we
+// collect from Kubernetes and other sources.  Each top-level "summary" object
+// is a description of a Kubernetes resource definition, and the PackagedObject
+// includes the additional OpenTelemetry resource and log record attributes.
+package graphpb
