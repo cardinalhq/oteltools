8cdce9f6049858e3ad6c496567b8aee3e5eac181
ruchirj <ruchir.jha@gmail.com>
2025-05-12 17:36:48 -0700

    Make sure the extra tags are part of tagValues

--- pkg/chqpb/percentile_sketch_test.go
+++ pkg/chqpb/percentile_sketch_test.go
@@ -91,14 +91,14 @@ func TestSketchCache_FlushAndGrouping(t *testing.T) {
 	spanA.Attributes().PutDouble(translate.CardinalFieldSpanDuration, 100)
 	spanA.Status().SetCode(ptrace.StatusCodeOk)
 	tagsA := map[string]string{"service.name": "auth"}
-	cache.Update("db.calls", "auth", "cluster", "namespace", tagsA, spanA)
+	cache.Update("db.calls", tagsA, spanA)
 
 	// Span B: billing service
 	spanB := ptrace.NewSpan()
 	spanB.Attributes().PutDouble(translate.CardinalFieldSpanDuration, 200)
 	spanB.Status().SetCode(ptrace.StatusCodeOk)
 	tagsB := map[string]string{"service.name": "billing"}
-	cache.Update("db.calls", "billing", "cluster", "namespace", tagsB, spanB)
+	cache.Update("db.calls", tagsB, spanB)
 
 	// Flush manually
 	cache.flush()
