eb5c1f649c5cc4d13d325d55301c6d25a2324e67
ruchirj <ruchir.jha@gmail.com>
2025-06-09 17:55:38 -0700

    Adding isAggregate to span sketches

--- pkg/chqpb/span_sketch.go
+++ pkg/chqpb/span_sketch.go
@@ -110,6 +110,7 @@ func (c *SpanSketchCache) Update(
 	metricName string,
 	tagValues map[string]string,
 	span ptrace.Span,
+	isAggregate bool,
 	resource pcommon.Resource,
 ) {
 	interval := span.EndTimestamp().AsTime().Truncate(c.interval).Unix()
@@ -131,6 +132,7 @@ func (c *SpanSketchCache) Update(
 			ExceptionCount:     0,
 			ExceptionsMap:      make(map[int64]string),
 			ExceptionCountsMap: make(map[int64]int64),
+			IsAggregate:        isAggregate,
 		}
 		ps, _ := ddsketch.NewDefaultDDSketch(0.01)
 		entry = &spanSketchEntry{
