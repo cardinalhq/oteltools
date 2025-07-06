cc21d01662fb2f3bf6a9088cc267296c99ea43dd
ruchirj <ruchir.jha@gmail.com>
2025-07-05 20:19:35 -0700

    Include serviceId in fingerprint

--- pkg/fingerprinter/spans.go
+++ pkg/fingerprinter/spans.go
@@ -15,13 +15,48 @@
 package fingerprinter
 
 import (
+	"github.com/cardinalhq/oteltools/hashutils"
 	"github.com/cardinalhq/oteltools/pkg/ottl/functions"
+	"github.com/cardinalhq/oteltools/pkg/translate"
 	"github.com/cespare/xxhash/v2"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.30.0"
 	"strconv"
 	"strings"
 )
+
+const (
+	serviceNameKey   = string(semconv.ServiceNameKey)
+	clusterNameKey   = string(semconv.K8SClusterNameKey)
+	namespaceNameKey = string(semconv.K8SNamespaceNameKey)
+)
+
+func ComputeExemplarKey(rl pcommon.Resource, extraKeys []string) ([]string, int64) {
+	keys := []string{
+		clusterNameKey, GetFromResource(rl.Attributes(), clusterNameKey),
+		namespaceNameKey, GetFromResource(rl.Attributes(), namespaceNameKey),
+		serviceNameKey, GetFromResource(rl.Attributes(), serviceNameKey),
+	}
+	keys = append(keys, extraKeys...)
+	return keys, int64(hashutils.HashStrings(nil, keys...))
+}
+
+func GetFromResource(attr pcommon.Map, key string) string {
+	clusterVal, clusterFound := attr.Get(key)
+	if !clusterFound {
+		return "unknown"
+	}
+	return clusterVal.AsString()
+}
+
+func GetFingerprintAttribute(l pcommon.Map) int64 {
+	fnk := translate.CardinalFieldFingerprint
+	if fingerprintField, found := l.Get(fnk); found {
+		return fingerprintField.Int()
+	}
+	return 0
+}
 
 func GetExceptionMessage(sr ptrace.Span) string {
 	var exceptionMessage string
@@ -51,8 +86,13 @@ func GetExceptionMessage(sr ptrace.Span) string {
 	return exceptionMessage
 }
 
-func CalculateSpanFingerprint(sr ptrace.Span, fpr Fingerprinter) int64 {
+func CalculateSpanFingerprint(res pcommon.Resource, sr ptrace.Span, fpr Fingerprinter) int64 {
 	fingerprintAttributes := make([]string, 0)
+	clusterName := GetFromResource(res.Attributes(), clusterNameKey)
+	namespaceName := GetFromResource(res.Attributes(), namespaceNameKey)
+	serviceName := GetFromResource(res.Attributes(), serviceNameKey)
+	fingerprintAttributes = append(fingerprintAttributes, clusterName, namespaceName, serviceName)
+
 	exceptionMessage := GetExceptionMessage(sr)
 	if exceptionMessage != "" {
 		computedFp, _, _, err := fpr.Fingerprint(exceptionMessage)
@@ -70,5 +110,4 @@ func CalculateSpanFingerprint(sr ptrace.Span, fpr Fingerprinter) int64 {
 	statusCodeStr := sr.Status().Code().String()
 	fingerprintAttributes = append(fingerprintAttributes, statusCodeStr)
 	return int64(xxhash.Sum64String(strings.Join(fingerprintAttributes, "##")))
-
 }
